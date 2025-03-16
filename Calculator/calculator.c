6dec836495bb49bd2dcf072ed91d83de2e57285f
Rachit Gupta <154494800+rdroid-27@users.noreply.github.com>
2025-03-16 16:54:24 +0000

    Complete basic calculator with code optimization and error handling.

--- Calculator/calculator.c
+++ Calculator/calculator.c
@@ -2,7 +2,7 @@
 
 void main() {
 
-    int input1=0, input2=0, output=0;
+    float input1=0, input2=0, output=0;
     char opt, cont_calc;
 
 
@@ -10,42 +10,54 @@ void main() {
     
     do
     {
-      // Take Operands as user input
-    printf("Enter Number 1: ");
-    scanf("%d", &input1);
-    printf("Enter Number 2: ");
-    scanf("%d", &input2);
+        // Take Operands as user input
+        printf("Enter Number 1: ");
+        scanf("%f", &input1);
+        printf("Enter Number 2: ");
+        scanf("%f", &input2);
 
-    // Take operator as user input
-    printf("Select the operation: \n");
-    printf("1. + \n2. - \n3. * \n4. / \nEnter Your Input: ");
-    scanf("%c", &opt);
+        // Take operator as user input
+        printf("Select the operation: \n");
+        printf("1. + \n2. - \n3. * \n4. / \nEnter Your Input: ");
+        scanf("\n%c", &opt);
 
-    switch (opt)
-    {
-    case '+':
-        output= input1 + input2;
-        break;
-    case '-':
-        output= input1 - input2;
-        break;
-    case '*':
-        output= input1 * input2;
-        break;
-    case '/':
-        output= input1 + input2;
-        break;
-    
-    default: printf("Invalid Operation! Exiting!");
-        return;
-        break;
-    }
+        // Perform Calculations based on user input
+        switch (opt)
+        {
+        case '+':
+            output= input1 + input2;
+            break;
+        case '-':
+            output= input1 - input2;
+            break;
+        case '*':
+            output= input1 * input2;
+            break;
+        case '/':
+            if(input2 == 0){
+                printf("Invalid Operation! Divide By 0!\n");
+                goto another_calc;
+            }
+            output= input1 / input2;
+            break;
+        
+        default: printf("Invalid Operation! Please select a valid Operator!\n");
+                goto another_calc;
+            break;
+        }
 
-    printf("RESULT: %d \n", output);
+        // Print the result
+        printf("RESULT: %f %c %f = %f\n", input1, opt, input2, output);
 
-    printf("Do another calculation? (Y/n): ");
-    scanf("%c", cont_calc);
+        // Perform another calculation
+        another_calc:
+        printf("Do another calculation? (Y/n): ");
+        scanf("\n%c", &cont_calc);
+
     } while (cont_calc == 'Y' | cont_calc == 'y');
+   
+    printf("Thank you for using THE CALCULATOR! Goodbye!\n");
+    return;
     
    
 }
