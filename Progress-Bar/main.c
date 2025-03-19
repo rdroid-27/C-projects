3b3dbf166aec5236d11a73d7fe049bc9f2a5ada8
Rachit Gupta <154494800+rdroid-27@users.noreply.github.com>
2025-03-19 14:18:49 +0000

    Add functions for variable and dynamic progress bar.

--- Progress-Bar/main.c
+++ Progress-Bar/main.c
@@ -1,32 +1,62 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
-const int BAR_LENGTH = 50;
+const int BAR_LENGTH = 100;
+const int MAX_TASKS = 5;
 
 typedef struct
 {
     int id;
     int progress;
-    int speed;
-}Task;
+    int step;
+} Task;
 
-void print_bar(int id, int progress);
+void print_bar(Task task);
+void clear_screen();
 
 void main()
 {
-    int id = 3, progress = 40;
-    print_bar(1, 25);
-    print_bar(3, 75);
-    print_bar(5, 65);
-    print_bar(0, 55);
-    print_bar(9, 0);
+    srand(time(NULL));
+    Task tasks[MAX_TASKS];
+
+    for (int i = 0; i < MAX_TASKS; i++)
+    {
+        tasks[i].id = i + 1;
+        tasks[i].progress = 0;
+        tasks[i].step = rand() % (10) + 1;
+    }
+
+    int task_incomplete = 1;
+
+    while (task_incomplete)
+    {
+        sleep(1);
+        clear_screen();
+        task_incomplete = 0;
+        for (int i = 0; i < MAX_TASKS; i++)
+        {
+            tasks[i].progress += tasks[i].step;
+            if (tasks[i].progress > 100)
+            {
+                tasks[i].progress = 100;
+            }
+            else
+                task_incomplete = 1;
+
+            print_bar(tasks[i]);
+        }
+    }
+    printf("\nALL TASKS COMPLETED!\n");
 }
 
-void print_bar(int id, int progress)
+void print_bar(Task task)
 {
 
-    int bars_to_show = (progress * BAR_LENGTH) / 100;
+    int bars_to_show = (task.progress * BAR_LENGTH) / 100;
 
-    printf("Task %d:  [", id);
+    printf("Task %d:  [", task.id);
     for (int i = 0; i < BAR_LENGTH; i++)
     {
         if (i < bars_to_show)
@@ -34,5 +64,14 @@ void print_bar(int id, int progress)
         else
             printf(" ");
     }
-    printf("] %d%%\n", progress);
+    printf("] %d%%\n", task.progress);
+}
+
+void clear_screen()
+{
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
 }
